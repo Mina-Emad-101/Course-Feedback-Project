97303d11d614c8da65afaa3ef1129a9cbdb410f7
Mina Emad <menamanos@gmail.com>
2025-05-05 12:16:43 +0300

    password hash

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,6 @@ VALUES
 
 INSERT INTO users (name, email, password, role_id)
 VALUES
-    ('Admin', 'admin@gmail.com', 'password', 1),
-    ('Instructor', 'instructor@gmail.com', 'password', 2),
-    ('Student', 'student@gmail.com', 'password', 3);
+    ('Admin', 'admin@gmail.com', '$2y$12$sVi5nffI3a/dgdY7ICq.re5kOG80ovoSE9LVnM0z9rPQXaFJDH53e', 1),
+    ('Instructor', 'instructor@gmail.com', '$2y$12$sVi5nffI3a/dgdY7ICq.re5kOG80ovoSE9LVnM0z9rPQXaFJDH53e', 2),
+    ('Student', 'student@gmail.com', '$2y$12$sVi5nffI3a/dgdY7ICq.re5kOG80ovoSE9LVnM0z9rPQXaFJDH53e', 3);
